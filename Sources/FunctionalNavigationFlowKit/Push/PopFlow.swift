c428c352fb256be1f0fe6ee144a46941cc53af8c
Ernest <magatar136@yandex.ru>
2021-04-12 23:06:02 +0300

    Добавляет PopFlow

---
+++ Sources/FunctionalNavigationFlowKit/Push/PopFlow.swift
@@ -0,0 +1,41 @@
+//
+//  PopFlow.swift
+//  
+//
+//  Created by Бабаян Эрнест on 12.04.2021.
+//
+
+import UIKit
+
+
+public func PopFlow(
+    in navigationController: UINavigationController,
+    animated: Bool = true
+) -> Flow {
+    onMainThread {
+        navigationController.popViewController(animated: animated)
+    }
+}
+
+
+public func PopFlow(
+    in navigationController: UINavigationController,
+    animated: Bool = true,
+    to viewController: UIViewController
+) -> Flow {
+    onMainThread {
+        navigationController.popToViewController(
+            viewController,
+            animated: animated
+        )
+    }
+}
+
+public func PopToRootFlow(
+    in navigationController: UINavigationController,
+    animated: Bool = true
+) -> Flow {
+    onMainThread {
+        navigationController.popToRootViewController(animated: animated)
+    }
+}
